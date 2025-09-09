1e19adda2d17ec537db4df42208a0001a53d9c9f
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-09 18:10:10 +0330

    Create UserRepository class for handling user data

---
+++ Kanban-ToDoList.DataLayer/Services/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿// System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Internal
+using Kanban_ToDoList.DataLayer.Model;
+using Kanban_ToDoList.DataLayer.Repository;
+
+
+namespace Kanban_ToDoList.DataLayer.Services
+{
+    public class UserRepository : IUsersRepository
+    {
+        public bool AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> FilterUserByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int LoginUserByUsernameAndPassword(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
