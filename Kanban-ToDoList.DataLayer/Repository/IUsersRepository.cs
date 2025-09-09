f14ef738ec4b441072c03280d0f9c96f71cd0c2c
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-09 17:16:47 +0330

    Create UserRepository interface

---
+++ Kanban-ToDoList.DataLayer/Repository/IUsersRepository.cs
@@ -0,0 +1,20 @@
+﻿// System
+using System.Collections.Generic;
+
+
+// Internal
+using Kanban_ToDoList.DataLayer.Model;
+
+
+namespace Kanban_ToDoList.DataLayer.Repository
+{
+    public interface IUsersRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        IEnumerable<User> FilterUserByUsername(string username);
+        bool AddUser(User user);  
+        User GetUserById(int userId);
+        int LoginUserByUsernameAndPassword(string username, string password);
+
+    }
+}
