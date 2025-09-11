2e4e87184bf41df17ee944766ee5019d92dd356a
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-11 14:04:04 +0330

    Create UserPermissionsRepository class
    -- Add method to AddUserPermission

---
+++ Kanban-ToDoList.DataLayer/Services/UserPermissionsRepository.cs
@@ -0,0 +1,34 @@
+﻿// System
+
+
+
+// Internal
+using Kanban_ToDoList.DataLayer.Model;
+using Kanban_ToDoList.DataLayer.Repository;
+
+
+
+namespace Kanban_ToDoList.DataLayer.Services
+{
+    public class UserPermissionsRepository : IUserPermissionsRepository
+    {
+        private KanbanToDoListWPFEntities db;
+        public UserPermissionsRepository(KanbanToDoListWPFEntities context)
+        {
+            db = context;
+        }
+        public bool AddUserPermission(UserPermission userPermission)
+        {
+            try
+            {
+                db.UserPermissions.Add(userPermission);
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+    }
+}
