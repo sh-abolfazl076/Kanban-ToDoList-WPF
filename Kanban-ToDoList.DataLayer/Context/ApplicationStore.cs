ac440996191e5624703a174586139ab7b73ebc07
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-13 13:33:02 +0330

    Add UnitOfWork pattern Implementation
    -- Added UsersRepository , TasksRepository , UserPermissionsRepository , Save and Dispose method

---
+++ Kanban-ToDoList.DataLayer/Context/ApplicationStore.cs
@@ -0,0 +1,73 @@
+﻿// System
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+// Internal
+
+
+
+namespace Kanban_ToDoList.App.Context
+{
+    public class ApplicationStore
+    {
+        private static readonly Lazy<ApplicationStore> _instance =
+            new Lazy<ApplicationStore>(() => new ApplicationStore());
+
+        private ApplicationStore() { }
+
+        public static ApplicationStore Instance => _instance.Value;
+
+        public string ServerName { get; private set; }
+        public string DatabaseName { get; private set; }
+        public string DbUsername { get; private set; }
+        public string DbPassword { get; private set; }
+
+        public void SetConnectionInfo(string server, string database, string username, string password)
+        {
+            ServerName = server;
+            DatabaseName = database;
+            DbUsername = username;
+            DbPassword = password;
+        }
+
+        public string RawConnectionString =>
+            $"Data Source={ServerName};Initial Catalog={DatabaseName};User ID={DbUsername};Password={DbPassword};Encrypt=False;";
+
+        public string EfConnectionString =>
+            $@"metadata=res://*/Model.KanbanModel.csdl|
+               res://*/Model.KanbanModel.ssdl|
+               res://*/Model.KanbanModel.msl;
+               provider=System.Data.SqlClient;
+               provider connection string=""{RawConnectionString};
+               MultipleActiveResultSets=True;
+               App=EntityFramework""";
+
+        public DbCompiledModel ConnectionString { get; internal set; }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(RawConnectionString))
+                {
+                    conn.Open();
+                    MessageBox.Show(EfConnectionString);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Kanban-ToDoList.DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,75 @@
+﻿// System
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Internal
+using Kanban_ToDoList.DataLayer.Model;
+using Kanban_ToDoList.DataLayer.Repository;
+using Kanban_ToDoList.DataLayer.Services;
+
+
+
+namespace Kanban_ToDoList.DataLayer.Context
+{
+    public class UnitOfWork
+    {
+        KanbanToDoListWPFEntities db = new KanbanToDoListWPFEntities();
+
+        private IUsersRepository _usersRepository;
+        public IUsersRepository UsersRepository
+        {
+            get
+            {
+                if (_usersRepository == null)
+                {
+                    _usersRepository = new UsersRepository(db);
+                }
+                return _usersRepository;
+            }
+        }// End
+
+        private ITasksRepository _tasksRepository;
+        public ITasksRepository TasksRepository
+        {
+            get
+            {
+                if (_tasksRepository == null)
+                {
+                    _tasksRepository = new TasksRepository(db);
+                }
+                return _tasksRepository;
+            }
+        }// End
+
+        private IUserPermissionsRepository _userPermissionsRepository;
+        public IUserPermissionsRepository UserPermissionsRepository
+        {
+            get
+            {
+                if (_userPermissionsRepository == null)
+                {
+                    _userPermissionsRepository = new UserPermissionsRepository(db);
+                }
+                return _userPermissionsRepository;
+            }
+        }// End
+
+
+
+        //
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+        //
+    }
+}
