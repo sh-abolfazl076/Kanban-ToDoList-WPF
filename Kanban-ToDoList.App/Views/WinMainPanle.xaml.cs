0ec453bd8e89e56dc8d2ae9311c34f23b1df8c63
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-19 10:54:16 +0330

    Event to open Add Task window in main panel

--- Kanban-ToDoList.App/Views/WinMainPanle.xaml.cs
+++ Kanban-ToDoList.App/Views/WinMainPanle.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// System
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+// Internal
+
+
 namespace Kanban_ToDoList.App.Views
 {
     /// <summary>
@@ -22,6 +27,17 @@ namespace Kanban_ToDoList.App.Views
         public WinMainPanle()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This event is used to open the "Add Task" window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreatedTask_Click(object sender, RoutedEventArgs e)
+        {
+            WinCreateTask addTask = new WinCreateTask();
+            addTask.ShowDialog();
         }
     }
 }
