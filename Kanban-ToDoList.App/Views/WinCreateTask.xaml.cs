607f2e10ef713408265f0e3d487dac018948ed72
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-19 11:24:03 +0330

    Load and set usernames from database in createTask

--- Kanban-ToDoList.App/Views/WinCreateTask.xaml.cs
+++ Kanban-ToDoList.App/Views/WinCreateTask.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// System
+using Kanban_ToDoList.DataLayer.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+// Internal
+
+
 namespace Kanban_ToDoList.App.Views
 {
     /// <summary>
@@ -23,5 +29,26 @@ namespace Kanban_ToDoList.App.Views
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Load and set usernames from the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (UnitOfWork db = new UnitOfWork(ApplicationStore.Instance.EfConnectionString))
+                {
+                    dgvGetUsers.ItemsSource = db.UsersRepository.GetAllUsers();
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("ِDatabes Error.");
+            }
+        }
     }
+
 }
