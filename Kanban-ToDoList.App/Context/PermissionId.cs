bc80a58d35d38ba42812251ce62c856614b44918
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-09-25 15:08:42 +0330

    Create PermissionId static class to store permission names

---
+++ Kanban-ToDoList.App/Context/PermissionId.cs
@@ -0,0 +1,16 @@
+﻿// System
+
+
+// Internal
+
+namespace Kanban_ToDoList.App.Context
+{
+    public static class PermissionId
+    {
+        static int AddTask = 1;
+        static int RemoveTask = 3;
+        static int ModifyTask = 5;
+        static int AccessUsers = 6;
+
+    }
+}
